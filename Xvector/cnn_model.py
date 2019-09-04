09056380c4fe4a23f62534bc3e0136197669245c
yaniv <opoo16@gmail.com>
2019-09-05 00:29:45 +0300

    cnn model add

---
+++ Xvector/cnn_model.py
@@ -0,0 +1,40 @@
+import keras
+from keras.layers import Conv2D, MaxPool2D, Input, Dense, Flatten, BatchNormalization
+from keras import Model
+
+
+class Cnn:
+    def __init__(self, data_dim=(512, 299), activation='relu', initializer='glorot_uniform', layer_size=[10,16,20,100]):
+        self.data_dim = data_dim
+        self.layer_size = layer_size
+        self.activation = activation
+        self.initializer = initializer
+        self.model = self.create_model()
+
+    def create_model(self):
+        inputs = Input(shape=(*self.data_dim, 1))
+        conv1 = Conv2D(filters=self.layer_size[0], kernel_size=(3, 3), activation=self.activation,
+                       kernel_initializer=self.initializer, name='conv1')(inputs)
+        conv1 = BatchNormalization()(conv1)
+        maxpool1 = MaxPool2D(pool_size=(2, 2))(conv1)
+        conv2 = Conv2D(filters=self.layer_size[1], kernel_size=(3, 3), activation=self.activation,
+                       kernel_initializer=self.initializer, name='conv2')(maxpool1)
+        conv2 = BatchNormalization()(conv2)
+        maxpool2 = MaxPool2D(pool_size=(2, 2))(conv2)
+        flat = Flatten()(maxpool2)
+        # avg_pool = GlobalAveragePooling2D()(maxpool2)
+        dense = Dense(units=self.layer_size[2], activation=self.activation, kernel_initializer=self.initializer,
+                      name='dense')(flat)
+        dense = BatchNormalization()(dense)
+
+        # avg_pool = GlobalAveragePooling2D()(conv3)
+
+        output = Dense(units=self.layer_size[3], activation='sigmoid', kernel_initializer=self.initializer,
+                       name='output')(dense)
+        model = Model(inputs, output)
+        model.summary()
+        return model
+
+
+if __name__ == '__main__':
+    cnn = Cnn()
